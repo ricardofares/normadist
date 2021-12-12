eaee1bf68fdf63e13e28d2459ee767e8a0e48c81
rricardof <ricardoxdd28@outlook.com>
2021-12-12 12:23:10 -0300

    Added the ErrorFunction type.
    * Represents a function that receives an argument a value x and returns the value erf(x).
    
    - The erf(x) represents the error function applied at x.

---
+++ src/erf.ts
@@ -0,0 +1,5 @@
+/**
+ * Represents the type of a {@link ErrorFunction} that represents
+ * a function receiving as argument a value x and returns the erf(x).
+ */
+export type ErrorFunction = (x: number) => number
