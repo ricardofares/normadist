062a0b92d15c36ccadaa200949253c32570182af
rricardof <ricardoxdd28@outlook.com>
2021-12-13 10:19:29 -0300

    Added an error function approximation using Chebyshev's approximation
    * The Chebyshev's approximation has a relative error of 1.2e-7.
    
    * The implemented routine of the Chebyshev's approximation is provided in the book Numerical Recipes, The Art of Scientific Computing, 3rd edition.

--- typescript/src/erf.ts
+++ typescript/src/erf.ts
@@ -7,6 +7,48 @@ import { factorial } from './factorial'
 export type ErrorFunction = (x: number) => number
 
 /**
+ * The error function approximation by the Chebyshev's Formula, receives a floating-point
+ * number x and returns an approximation for the value erf(x).
+ *
+ * The formula for approximation function is provided in the book
+ * Numerical Recipes, The Art of Scientific Computing, 3rd edition.
+ *
+ * The relative error bound for that approximation is: 1.2e-7. The source of that
+ * information is in the book cited above, as well.
+ *
+ * @param {Number} x the function argument
+ *
+ * @returns {Number} an approximation using Chebyshev's Formula for the value of erf(x).
+ */
+export function chebyshevErf(x: number): number {
+  const z: number = Math.abs(x)
+  const t: number = 2.0 / (2.0 + z)
+  const r: number =
+    t *
+    Math.exp(
+      -z * z -
+        1.26551223 +
+        t *
+          (1.00002368 +
+            t *
+              (0.37409196 +
+                t *
+                  (0.09678418 +
+                    t *
+                      (-0.18628806 +
+                        t *
+                          (0.27886807 +
+                            t *
+                              (-1.13520398 +
+                                t *
+                                  (1.48851587 +
+                                    t * (-0.82215223 + t * 0.17087277))))))))
+    )
+
+  return x >= 0 ? 1 - r : r - 1
+}
+
+/**
  * The error function formula approximation by a Taylor Polynomial with
  * a specified degree that receives a floating-pointer number x and returns
  * an approximation for the value erf(x).
