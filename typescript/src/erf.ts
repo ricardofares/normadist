eeebfebb146a78b00582ff73f347f96ef48f9fd6
rricardof <ricardoxdd28@outlook.com>
2021-12-12 21:53:42 -0300

    Added an error function approximation using Taylor Polynomial.
    * The degree of the taylor polynomial for that approximation is set as default to 15.
    
    * The approximation of erf(x) using Taylor Polynomial is higher as the degree is.
    
    * There is a maximum degree in the approximation using Taylor Polynomial that the floating-pointer arithmetic can handle, after that maximum the accuracy maintains constant.

--- typescript/src/erf.ts
+++ typescript/src/erf.ts
@@ -1,5 +1,36 @@
+import { factorial } from './factorial'
+
 /**
  * Represents the type of a {@link ErrorFunction} that represents
  * a function receiving as argument a value x and returns the erf(x).
  */
 export type ErrorFunction = (x: number) => number
+
+/**
+ * The error function formula approximation by a Taylor Polynomial with
+ * a specified degree that receives a floating-pointer number x and returns
+ * an approximation for the value erf(x).
+ *
+ * If the degree is not specified, then the default degree is set to 15.
+ *
+ * The approximation of erf(x) by this function is higher as the degree is.
+ *
+ * @param {Number} x the function argument
+ * @param {Number} degree the Taylor Polynomial degree, the default degree
+ *                        is set to 15.
+ *
+ * @returns {Number} an approximation using Taylor Polynomial for the value of erf(x).
+ */
+export function taylorErf(x: number, degree: number = 15): number {
+  const a: number = 1.1283791670955126 * x
+  let b: number = 0
+
+  for (let index = 0; index <= degree; index++) {
+    const t: number = index << 1
+    const s: number = Math.pow(-1, index)
+
+    b += (s * Math.pow(x, t)) / (factorial(index) * (t + 1))
+  }
+
+  return a * b
+}
