e5d8048eaf568748f9037c68f43bee0173663632
rricardof <ricardoxdd28@outlook.com>
2021-12-12 22:18:33 -0300

    Added an error function approximation using Vazquez-Leal formula
    * The Vazquez-Leal approximation has a relative error bound of 1.88e-4. The source for that information is provided at https://arxiv.org/ftp/arxiv/papers/2012/2012.04466.pdf

--- typescript/src/erf.ts
+++ typescript/src/erf.ts
@@ -36,3 +36,26 @@ export function taylorErf(x: number, degree: number = 15): number {
 
   return a * b
 }
+
+/**
+ * The error function formula approximation by Vazquez-Leal that receives
+ * a floating-point number x and returns an approximation for the value erf(x).
+ *
+ * The approximation for the error function is provided in their article:
+ * https://www.uv.mx/personal/hvazquez/files/2012/02/124029.pdf
+ *
+ * The relative error bound for that approximation is: 1.88e-4. The source of
+ * this information is provided in:
+ * https://arxiv.org/ftp/arxiv/papers/2012/2012.04466.pdf
+ *
+ * @param {Number} x the function argument
+ *
+ * @returns {Number} an approximation using Vazquez-Leal for the value of erf(x).
+ */
+export function vazquezLealErf(x: number): number {
+  const a: number = 11.001696879181248
+  const b: number = 0.17789761643397722
+  const c: number = 55.5
+
+  return Math.tanh(a * x - c * Math.atan(b * x))
+}
