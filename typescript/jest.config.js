658fa0309743bd1252d885b768a4dc26bb5285b3
rricardof <ricardoxdd28@outlook.com>
2021-12-18 12:33:04 -0300

    Added the support for path mapping to ts-jest

--- typescript/jest.config.js
+++ typescript/jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   roots: ['./tests'],
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/']
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 }
