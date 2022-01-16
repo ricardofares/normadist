c81cb42561892b8938273620f1171b1184425d91
rricardof <ricardoxdd28@outlook.com>
2022-01-16 18:27:22 -0300

    Removed the removed error function approximations from the error function approximation test.

--- typescript/tests/erf.test.ts
+++ typescript/tests/erf.test.ts
@@ -1,31 +1,11 @@
-import {
-  ErrorFunction,
-  chebyshevErf,
-  taylorErf,
-  vazquezLealErf,
-  soranzoErf
-} from '@/util/erf'
-
-/**
- * This array contains all functions that represents the
- * approximation formulas for the error function erf(x).
- */
-const erfs: ErrorFunction[] = [
-  chebyshevErf,
-  taylorErf,
-  vazquezLealErf,
-  soranzoErf
-]
+import { erf } from '@/util/erf'
 
 describe('checking erf(x) values', () => {
-  erfs.forEach((erf) => {
-    it(`must return 0 for ${erf.name}(0)`, () => {
-      expect(erf(0.0)).toEqual(0.0)
-    })
+  it(`must return 0 for ${erf.name}(0)`, () => {
+    expect(erf(0.0)).toEqual(0.0)
+  })
 
-    if (erf !== taylorErf)
-      it(`must return 1 for ${erf.name}(+Infinity)`, () => {
-        expect(erf(Number.POSITIVE_INFINITY)).toEqual(1.0)
-      })
+  it(`must return 1 for ${erf.name}(+Infinity)`, () => {
+    expect(erf(Number.POSITIVE_INFINITY)).toEqual(1.0)
   })
 })
